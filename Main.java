ef37c445d6e972a4b6088cc26b71037381e8179f
AnthonyGrod <tantien02@hotmail.com>
2023-04-13 21:40:07 +0200

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Tutaj byłaby pętla for iterująca się po dniach trwania Giełdy, a w jej środku byłyby w odpowiedniej
+        // sekwencji wywołane metody z klasy Symuluj. Nie ma to jednak prawa działać ze względu na brak wczytywania
+        // więc nie umieszczam do tego kodu tutaj.
+    }
+}
